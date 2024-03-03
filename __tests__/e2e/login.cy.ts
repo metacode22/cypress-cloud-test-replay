c01e060fe8fb12a2a49bfbbc64c43307640d49cb
metacode22 <alohajune22@gmail.com>
2024-03-03 19:03:09 +0900

    chore: baseUrl 설정

--- __tests__/e2e/login.cy.ts
+++ __tests__/e2e/login.cy.ts
@@ -1,6 +1,6 @@
 describe("로그인", () => {
   it("로그인에 성공하면 Home으로 이동합니다.", () => {
-    cy.visit("http://localhost:3000/login");
+    cy.visit("/login");
 
     cy.get("input[type=email]").type("cypress@test.replay");
     cy.get("input[type=password]").type("test1234");
--- cypress.config.ts
+++ cypress.config.ts
@@ -3,5 +3,6 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     specPattern: "__tests__/e2e",
+    baseUrl: "http://localhost:3000",
   },
 });
